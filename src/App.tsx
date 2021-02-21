1be536c0351cc44e630862692c79cdbf29feadc0
Selphyz <carlos.dom13@hotmail.com>
2021-02-21 04:33:14 +0100

    cambio de imagen de fondo

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { About, Home, NavBar, Post, Project, SinglePost } from './components';
+import bg from './img/fondocustom.jpg';
 const App = () => {
   return (
     <BrowserRouter>
+      <img src={bg} alt='background' className='absolute object-cover w-full h-full' />
       <NavBar />
       <Switch>
         <Route component={Home} path='/' exact />
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,7 +3,7 @@ import bg from '../img/background.jpg';
 const Home = () => {
   return (
     <main>
-      <img src={bg} alt='background' className='absolute object-cover w-full h-full' />
+      {/* <img src={bg} alt='background' className='absolute object-cover w-full h-full' /> */}
       <section className='relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8'>
         <h1 className='simple text-6xl text-black font-bold cursive leading-none lg:leading-snug home-name'>
           Bienvenido a mi Blog
